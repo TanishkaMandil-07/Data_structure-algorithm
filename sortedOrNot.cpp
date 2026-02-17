a11755dcf817f4dfbca9eccba546f865af06c200
TanishkaMandil-07 <tanishkamandil4@gmail.com>
2026-02-17 19:40:01 +0530

    Create sortedOrNot.cpp

---
+++ sortedOrNot.cpp
@@ -0,0 +1,26 @@
+//find array is sorted or not
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the size of array"<<endl;
+    cin>>n;
+    vector<int>a(n);
+    int largest=0;
+    int slargest=0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n-1;i++){
+     if(a[i]<=a[i+1]){
+         continue;
+     }
+     else{
+         cout<<"not sorted";
+         break;
+     }
+    }
+    cout<<"sorted"<<endl;
+}
+    
