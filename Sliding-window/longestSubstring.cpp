1a4eb9eb0d5796320c2ce4d52ea5d23d26cd5907
TanishkaMandil-07 <tanishkamandil4@gmail.com>
2026-02-27 15:52:35 +0530

    Create longestSubstring.cpp

---
+++ Sliding-window/longestSubstring.cpp
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int low=0;
+        int high=0;
+        int n=s.size();
+        int res=0;
+        unordered_map<char,int>f;
+        for(high=0;high<n;high++){
+            f[s[high]]++;
+            int k=high-low+1;
+            while(f.size()<k){
+                 f[s[low]]--;
+
+                if(f[s[low]]==0){
+                    f.erase(s[low]);
+                }
+                low++;
+                k=high-low+1;
+            }
+            int len=high-low+1;
+            res=max(res,len);
+
+        }
+        return res;
+    }
+};
