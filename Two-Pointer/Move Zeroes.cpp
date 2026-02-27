763587cf62395badc781438657f4afd44082d249
TanishkaMandil-07 <tanishkamandil4@gmail.com>
2026-02-27 15:48:01 +0530

    Create Move Zeroes.cpp

---
+++ Two-Pointer/Move Zeroes.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/move-zeroes/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]==0){
+            count++;
+        }
+    }
+    nums.erase(remove(nums.begin(),nums.end(),0),nums.end());
+    for(int i=0;i<count;i++){
+        nums.push_back(0);
+    }
+    }
+};
