46f4ba4f94c46d41d561a0c6f4aba16e42a5e2c4
TanishkaMandil-07 <tanishkamandil4@gmail.com>
2026-02-27 15:50:56 +0530

    Create intersection2.cpp

---
+++ Two-Pointer/intersection2.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>res;
+         int i=0; int j=0;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        while(i<nums1.size()&&j<nums2.size()){
+            if(nums1[i]==nums2[j]){
+                res.push_back(nums2[j]);
+                i++;
+                j++;
+            }
+            else if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        return res;
+    }
+};
